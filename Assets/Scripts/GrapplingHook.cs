e60836cdeb1e9087a86e353744a44d22c6430833
SheriffWinchester <m3llpak@gmail.com>
2023-11-23 21:02:20 +0200

    Grappling hook

--- Assets/Scripts/GrapplingHook.cs
+++ Assets/Scripts/GrapplingHook.cs
@@ -4,15 +4,46 @@ using UnityEngine;
 
 public class GrapplingHook : MonoBehaviour
 {
-    // Start is called before the first frame update
+    Camera mainCamera;
+    bool grappleEnabled = false;
+    SpringJoint2D springJoint2D;
     void Start()
     {
-        
+        mainCamera = Camera.main;
+        springJoint2D = GetComponent<SpringJoint2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetMouseButton(0))
+        {
+            Vector2 mousePos = mainCamera.ScreenToWorldPoint(Input.mousePosition);   
+            Vector2 direction = new Vector2(mousePos.x - transform.position.x, mousePos.y - transform.position.y);
+            var hit = Physics2D.Linecast(transform.position, mousePos);
+
+            grappleEnabled = true;
+            springJoint2D.enabled = true;
+            springJoint2D.connectedAnchor = mousePos;
+            Debug.DrawLine(transform.position, mousePos);
+
+            if (hit.collider != null)
+            {
+                // _LineRenderer.SetPosition(0, transform.position);
+                // _LineRenderer.SetPosition(1, hit.transform.position);
+                // _LineRenderer.enabled = true;
+
+                Vector2 _force = transform.position - hit.transform.position;
+                Vector2 _normForce = Vector3.Normalize(_force);
+                hit.rigidbody.velocity = _normForce * 5.0f;
+            } 
+            // else 
+            // {
+            //     _LineRenderer.enabled = false;
+            // }
+        }
+        else {
+            springJoint2D.enabled = false;
+        }
     }
 }
